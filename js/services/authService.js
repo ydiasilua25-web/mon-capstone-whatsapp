b831865e626049e821758a6f5e0ad02d6152aee3
Yorgen Diasilua <ydiasilua25@gmail.com>
2026-07-01 23:26:35 +0100

    feat: implement user registration

---
+++ js/services/authService.js
@@ -0,0 +1,28 @@
+export async function register(user) {
+    try {
+        const response = await fetch("https://kadea-chat-api.onrender.com/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": "wksp_4ff9bde48b6dafc1faaae4792a3e6677"
+            },
+            body: JSON.stringify({
+                fullName: user.fullName,
+                email: user.email,
+                password: user.password
+            })
+        });
+
+        const data = await response.json();
+        
+
+       if (!response.ok) {
+        return data;
+        }
+
+        return data;
+
+    } catch (error) {
+        throw error;
+    }
+}
--- js/ui/register.js
+++ js/ui/register.js
@@ -1,3 +1,4 @@
+import { register } from "../services/authService.js";
 const form = document.querySelector("#registerForm");
 
 const fullNameInput = document.querySelector("#fullname");
@@ -5,26 +6,45 @@ const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const confirmPasswordInput = document.querySelector("#confirmPassword");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    console.log("Le formulaire a été envoyé !");
+    try {
+        const user = {
+            fullName: fullNameInput.value,
+            email: emailInput.value,
+            password: passwordInput.value,
+            confirmPassword: confirmPasswordInput.value,
+        };
 
-     const user = {
-        fullName: fullNameInput.value,
-        email: emailInput.value,
-        password: passwordInput.value,
-        confirmPassword: confirmPasswordInput.value,
-    };
+        if (
+            user.fullName.trim() === "" ||
+            user.email.trim() === "" ||
+            user.password.trim() === "" ||
+            user.confirmPassword.trim() === ""
+        ) {
+            alert("Veuillez remplir tous les champs");
+            return;
+        }
 
-    if (
-    user.fullName.trim() === "" ||
-    user.email.trim() === "" ||
-    user.password.trim() === "" ||
-    user.confirmPassword.trim() === ""
-) {
-    alert("Veuillez remplir tous les champs");
-    return;
-}
-console.log(user);
+        if (user.password !== user.confirmPassword) {
+            alert("Les mots de passe ne correspondent pas.");
+            return;
+        }
+        const data = await register(user);
+
+        if (data.success) {
+            alert(data.message);
+
+            //window.location.href = "login.html";
+        } else {
+            alert(data.message);
+        }
+
+    } catch (error) {
+
+        alert("Une erreur est survenue. Veuillez réessayer.");
+
+    }
 });
+
