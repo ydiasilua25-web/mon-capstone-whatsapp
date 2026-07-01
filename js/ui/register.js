a5cda77a87536c89004751effb16d1c3b43b385d
Yorgen Diasilua <ydiasilua25@gmail.com>
2026-07-01 01:54:14 +0100

    feat: je viens de gerer cote condition de user

--- js/ui/register.js
+++ js/ui/register.js
@@ -1,7 +1,30 @@
 const form = document.querySelector("#registerForm");
 
+const fullNameInput = document.querySelector("#fullname");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const confirmPasswordInput = document.querySelector("#confirmPassword");
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     console.log("Le formulaire a été envoyé !");
+
+     const user = {
+        fullName: fullNameInput.value,
+        email: emailInput.value,
+        password: passwordInput.value,
+        confirmPassword: confirmPasswordInput.value,
+    };
+
+    if (
+    user.fullName.trim() === "" ||
+    user.email.trim() === "" ||
+    user.password.trim() === "" ||
+    user.confirmPassword.trim() === ""
+) {
+    alert("Veuillez remplir tous les champs");
+    return;
+}
+console.log(user);
 });
